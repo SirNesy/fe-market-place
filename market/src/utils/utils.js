4bc5fe339cfc8f6ce8aa89ba47f7b1a8dbaeb732
nesy <umoizeonesi@gmail.com>
2023-01-26 10:44:07 +0000

    this commit removes sort basket

--- market/src/utils/utils.js
+++ market/src/utils/utils.js
@@ -1,24 +1,23 @@
-const sortBasket = (items) => {
-  const itemCopy = [];
-  const sortedItems = [];
-//   let countObj = {}
-  for (let i = 0; i < items.length; i++) {
-    let itemObj = { ...items[i] };
-    itemCopy.push({ itemObj });
-  }
+// const sortBasket = (items) => {
+//   const itemCopy = [];
+//   const sortedItems = [];
+// //   let countObj = {}
+//   for (let i = 0; i < items.length; i++) {
+//     let itemObj = { ...items[i] };
+//     itemCopy.push({ itemObj });
+//   }
 
-  for (let i = 0; i < itemCopy.length; i++){
-    let count = 0
-    if(itemCopy[i])
-  }
+//   for (let i = 0; i < itemCopy.length; i++){
+//     let count = 0
+//     if(itemCopy[i])
+//   }
 
-
-};
+// };
 
 //   let arr = [1,2,3,1,2,3,4];
 
 //   let countFunc = keys => {
 //     countObj[keys] = ++countObj[keys] || 1;
 //   }
-  
+
 //   arr.forEach(countFunc);
